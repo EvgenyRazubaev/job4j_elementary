0798ab5223cf450ccb6ab975e0c1115c1613bfd4
joniru111223@gmail.com <XTHysq7007jfpbc!!>
2023-03-24 15:30:53 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Разубаев Евгений");
+        System.out.println("30.07.1989");
+
+    }
+}
